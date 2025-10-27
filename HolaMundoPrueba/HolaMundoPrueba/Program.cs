9cc28ef852bc299b66c12e6097d38ba42d0716fb
Edwin Cisneros <edwincisneros2346@gmail.com>
2025-10-26 21:51:11 -0500

    Cambio nombre

--- HolaMundoPrueba/HolaMundoPrueba/Program.cs
+++ HolaMundoPrueba/HolaMundoPrueba/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hola edwin, esto es una prueba. Cambia el siguiente texto por el nombre de teran para saber " +
-    "que sirve. XXXXX es gay!");
+    "que sirve. TERAN es gay!");
--- HolaMundoPrueba/HolaMundoPrueba/obj/Debug/net8.0/HolaMundoPrueba.AssemblyInfo.cs
+++ HolaMundoPrueba/HolaMundoPrueba/obj/Debug/net8.0/HolaMundoPrueba.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HolaMundoPrueba")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b62f0b8542484cefd96169730475e5fba4a8edc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57af5306d2c28815a41b5dbb01d71be8ed3ff74b")]
 [assembly: System.Reflection.AssemblyProductAttribute("HolaMundoPrueba")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HolaMundoPrueba")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
